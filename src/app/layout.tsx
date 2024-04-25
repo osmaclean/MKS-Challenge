ca8bfb71fdc3bae0516c44c52a67f46bf9bbe02f
Maclean <contatolucasmaclean@gmail.com>
2024-04-25 16:09:03 -0300

    chore: initial setup (adjustable)

---
+++ src/app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from 'next'
+import { Montserrat } from 'next/font/google'
+import './globals.css'
+
+const montserrat = Montserrat({
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+})
+
+export const metadata: Metadata = {
+  title: 'MKS Sistemas',
+  description: 'Loja virtual de produtos eletrônicos',
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="pt-BR">
+      <body className={montserrat.className}>{children}</body>
+    </html>
+  )
+}
---
+++ src/styles/themes/default.ts
@@ -0,0 +1,7 @@
+export const defaultTheme = {
+  white: '#f6f6f6',
+
+  'blue-400': '#0F52BA',
+
+  'black-600': '#373737',
+}
